6061029be14beafa3e822e7c19bd695eafd88e7f
kellazo <gilli11519@gmail.com>
2015-10-19 09:50:26 +0100

    spc
    spc

---
+++ Dev_class6_solution_PC/Motor2D/j1Audio.h
@@ -0,0 +1,63 @@
+#ifndef __j1AUDIO_H__
+#define __j1AUDIO_H__
+
+#include "j1Module.h"
+
+#define DEFAULT_MUSIC_FADE_TIME 2.0f
+
+struct _Mix_Music;
+struct Mix_Chunk;
+
+class j1Audio : public j1Module
+{
+public:
+
+	j1Audio();
+
+	// Destructor
+	virtual ~j1Audio();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node&);
+
+	// Called before all Updates
+	bool PreUpdate();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Called before all Updates
+	bool PostUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Load / Save
+	bool Load(pugi::xml_node&);
+	bool Save(pugi::xml_node&) const;
+	
+	// Load / Save Mute
+	bool LoadMute(pugi::xml_node&);
+	bool SaveMute(pugi::xml_node&) const;
+
+	// Play a music file
+	bool PlayMusic(const char* path, float fade_time = DEFAULT_MUSIC_FADE_TIME);
+
+	// Load a WAV in memory
+	unsigned int LoadFx(const char* path);
+
+	// Play a previously loaded WAV
+	bool PlayFx(unsigned int fx, int repeat = 0);
+
+public:
+	int					volume_value;
+	bool				mute;
+	bool				just_muted;
+
+private:
+
+	_Mix_Music*			music = NULL;
+	p2List<Mix_Chunk*>	fx;
+};
+
+#endif // __j1AUDIO_H__
